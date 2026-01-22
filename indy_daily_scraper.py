ea8312cb134bcf330c70bbad1e29902cee3c1447
Elisabeth Gawthrop <esg2145@columbia.edu>
2026-01-21 21:56:19 -0500

    Update indy_daily_scraper.py

--- indy_daily_scraper.py
+++ indy_daily_scraper.py
@@ -20,9 +20,10 @@ station = 'KININDIA550'
 wx_url = f'https://www.wunderground.com/dashboard/pws/{station}/graph/{year}-{month}-{day}/{year}-{month}-{day}/daily'
 wx = requests.get(wx_url)
 doc = BeautifulSoup(wx.text, 'html.parser')
-rain_list = doc.findAll('span', class_='test-false wu-unit wu-unit-rain ng-star-inserted')
+precip_section = doc.find('span', class_='test-false wu-unit wu-unit-rain').parent
+rain_value = float(precip_section.find('span', class_='wu-value wu-value-to').text)
+#rain_list = doc.findAll('span', class_ = "wu-value wu-value-to")
 date = f'{month}:{day}:{year}'
-value = 1.2
 
 # check to see if csv already exists, and if not create a new one 
 path_to_file = 'daily.csv'
@@ -38,7 +39,8 @@ else:
 # open csv, add new data 
 df = pd.read_csv('daily.csv')
 df2 = {'Station': station, 'Date': date, 'Rainfall': value}
-df = df.append(df2, ignore_index = True)
+#df = df.append(df2, ignore_index = True)
+df = pd.concat([df, pd.DataFrame([df2])], ignore_index=True)
 
 # write new csv 
 df.to_csv('daily.csv', index = False)
